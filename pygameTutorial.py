462842f13b9afb11cf3ee47f1e676210d2e21ecc
Kimber <kimberly.winter@students.olin.edu>
2016-11-09 23:48:49 -0500

    Day18 Cata, sorry for being late :(

--- pygameTutorial.py
+++ pygameTutorial.py
@@ -10,19 +10,25 @@ black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("football.png")
-ballrect = ball.get_rect()
+#ball = pygame.image.load("football.png")
+#ballrect = ball.get_rect()
 
+class ball(object):
+    def __init__(self):
+        self.ball=pygame.image.load("football.png")
+        self.ballrect=self.ball.get_rect()
+
+ball1=ball()
 while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: 
             	sys.exit()
-        ballrect = ballrect.move(speed)
-        if ballrect.left < 0 or ballrect.right > width:
-            speed[0] = -speed[0]
-        if ballrect.top < 0 or ballrect.bottom > height:
-            speed[1] = -speed[1]
+#        ballrect = ballrect.move(speed)
+ #       if ballrect.left < 0 or ballrect.right > width:
+  #          speed[0] = -speed[0]
+   #     if ballrect.top < 0 or ballrect.bottom > height:
+    #        speed[1] = -speed[1]
 
         screen.fill(black)
-        screen.blit(ball, ballrect)
+        screen.blit(ball1.ball, ball1.ballrect)
         pygame.display.flip()
